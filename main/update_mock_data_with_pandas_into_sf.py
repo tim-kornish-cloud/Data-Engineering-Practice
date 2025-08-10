4414c064b1ec9cdcc8cd5d8fb9c518c681818c9b
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-08-10 08:47:55 -0700

    init commit for upsert/update scripts
    adding base of upsert and update script before adding comparison logic to determine what to update

---
+++ main/update_mock_data_with_pandas_into_sf.py
@@ -0,0 +1,69 @@
+"""
+Author: Timothy Kornish
+CreatedDate: August - 10 - 2025
+Description: Log into Salesforce and update a specific set of accounts
+             based on query criteria
+
+"""
+
+import numpy as np
+import pandas as pd
+import os
+from simple_salesforce import Salesforce
+from custom_sf_utilities import  Custom_SF_Utilities, Custom_Utilities
+from credentials import Credentials
+
+#create and instance of the custom salesforce utilities class used to interact with Salesforce
+SF_Utils = Custom_SF_Utilities()
+#create and instance of the custom utilities class used to format and modify dataframe data
+Utils = Custom_Utilities()
+# create instance of credentials class where creds are stored to load into the script
+Cred = Credentials()
+
+# declare which environment this script will perform operations against,
+# can have multiple environments in the same script at the same time
+environment = 'Dev'
+
+#number of records to delete
+num_of_records = 10
+
+# query string to select records from salesforce
+# before uploading with a delete  DML operation
+account_query = "SELECT Id FROM Account WHERE CreatedBy.Name = 'Timothy Kornish'"
+
+#set up directory pathway to load csv data and output fallout and success results to
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# set up fallout ans success path to save files to
+# success file path
+success_file = dir_path + "\\Output\\UPDATE\\SUCCESS_Update_" + environment + ".csv"
+# fallout file path
+fallout_file = dir_path + "\\Output\\UPDATE\\FALLOUT_Update_" + environment + ".csv"
+
+
+# load credentials for Salesforce and the Dev environement
+# I use this method instead of a hardcoding credentials and instead of a
+# properties or txt file due to parsing issues
+# The credentials are stored as strings in a dictionary attribute of a class
+
+# get username from credentials
+username = Cred.get_username("Salesforce", environment)
+# get password from credentials
+password = Cred.get_password("Salesforce", environment)
+# get login token from credentials
+token = Cred.get_token("Salesforce", environment)
+
+# create a instance of simple_salesforce to query and perform operations against salesforce with
+sf = SF_Utils.login_to_salesForce(username, password, token)
+
+# query salesforce and return the accounts to be deleted
+account_query_results = SF_Utils.query_salesforce(sf, account_query)
+# convert query results to a dataframe
+accounts_to_update_df = SF_Utils.load_query_with_lookups_into_DataFrame(account_query_results)
+# encode the dataframe before uploading to delete
+accounts_to_update_df = Utils.encode_df(accounts_to_update_df)
+
+print(accounts_to_update_df.head())
+
+# upload the records to salesforce
+SF_Utils.upload_records_to_salesforce(sf, accounts_to_update_df, 'Account', 'update', success_file, fallout_file)
