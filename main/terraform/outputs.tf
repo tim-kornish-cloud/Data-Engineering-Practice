59666382bee77a5994dbfde61ebb10ef4d109f7b
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-08-21 02:24:43 -0700

    Update outputs.tf
    updating outputs file to print ip of created ec2 instance

--- main/terraform/outputs.tf
+++ main/terraform/outputs.tf
@@ -4,6 +4,6 @@ output "bucket_name" {
 }
 
 output "instance_public_ip" {
-  value = aws_instance.pandas-ec2.instance_public_ip
+  value = aws_instance.pandas-ec2.public_ip
   description = "The public IP address of the EC2 instance"
 }
