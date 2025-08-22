65cde2266a8b64e77b8ae92e79a0680d0520ce12
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-08-22 12:30:48 -0700

    Update load_csv_into_pandas_into_s3.py
    cleaning up load csv into s3 script

--- main/load_csv_into_pandas_into_s3.py
+++ main/load_csv_into_pandas_into_s3.py
@@ -16,7 +16,7 @@ from custom_db_utilities import  EC2_S3_Utilities
 from credentials import Credentials
 
 # create and instance of the custom salesforce utilities class used to interact with Salesforce
-SF_Utils = EC2_S3_Utilities()
+AWS_Utils = EC2_S3_Utilities()
 # create instance of credentials class where creds are stored to load into the script
 Cred = Credentials()
 
@@ -32,14 +32,8 @@ num_of_records = 10
 # starting index to choose records
 record_start = 5
 
-# set up directory pathway to load csv data and output fallout and success results to
+# set up directory pathway to load csv data
 dir_path = os.path.dirname(os.path.realpath(__file__))
-
-# set up fallout ans success path to save files to
-# success file path
-success_file = dir_path + "\\Output\\DELETE\\SUCCESS_Delete_" + environment + "_" + database + ".csv"
-# fallout file path
-fallout_file = dir_path + "\\Output\\DELETE\\FALLOUT_Delete_" + environment + "_" + database + ".csv"
 
 # set input path for mock data csv
 input_csv_file = dir_path + ".\\MockData\\MOCK_DATA.csv"
@@ -51,45 +45,10 @@ mock_df = pd.read_csv(input_csv_file)
 # select only 10 records
 df_to_upload = mock_df.iloc[record_start:record_start+num_of_records]
 
-#log into boto3 with Credentials
-s3_client = boto3.client(service_name = 's3',
-                         region_name = Cred.get_region(dbms = database, env = environment),
-                         aws_access_key_id = Cred.get_access_key_id(dbms = database, env = environment),
-                         aws_secret_access_key = Cred.get_secret_access_key(dbms = database, env = environment))
-
-# s3 bucket name to upload csv to
+# set s3 bucket name to upload csv to
 s3_bucket_name = 'pandas-interface-bucket'
-# name of the csv file being uploaded to s3 bucket
+# set name of the csv file being uploaded to s3 bucket
 filename = 'MOCK_DATA_Multi_Data_Types_subset.csv'
-# create path string to upload csv
-s3_path = f"s3://{s3_bucket_name}/{filename}"
-# upload csv to s3 path
-df_to_upload.to_csv(s3_path, index = False)
-# print out showing success of upload
-print(f"DataFrame successfully saved to {s3_path}")
 
-
-
-
-
-
-# keeping here as first example code to modify from
-
-# import boto3
-# import pandas as pd
-# from io import BytesIO
-#
-# csv_buffer = BytesIO()
-# df.to_csv(csv_buffer, index=False)
-# csv_buffer.seek(0) # Rewind the buffer to the beginning
-#
-# s3_client = boto3.client(service_name = 's3', region_name=None,aws_access_key_id=None, aws_secret_access_key=None)
-# bucket_name = 'pandas-interface-bucket' # Replace with your S3 bucket name
-# filename = 'MOCK_DATA_Multi_Data_Types.csv' # Desired key (path and filename) in S3
-#
-# #
-# #s3_client.upload_fileobj(csv_buffer, bucket_name, filename)
-# #print(f"DataFrame uploaded to s3://{bucket_name}/{filename}")
-#
-#
-# df.to_csv('s3://pandas-interface-bucket/MOCK_DATA_Multi_Data_Types.csv', index=False)
+# upload dataframe to s3 bucket
+AWS_Utils.upload_dataframe_to_s3(df_to_upload, s3_bucket_name, filename)
