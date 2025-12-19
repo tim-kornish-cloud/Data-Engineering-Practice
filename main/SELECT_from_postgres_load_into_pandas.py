a18ff773c9237fcb8ec75eba0fc6a027f8d8161b
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-12-18 23:35:06 -0700

    Update SELECT_from_postgres_load_into_pandas.py
    removing hardcoded login credentials before making more complex edits for postgres

--- main/SELECT_from_postgres_load_into_pandas.py
+++ main/SELECT_from_postgres_load_into_pandas.py
@@ -11,14 +11,33 @@ import psycopg2
 from datetime import datetime
 # import random generator
 import random
+# retreive stored credentials
+from credentials import Credentials
+
+# setting database flavor to postgres for
+database = "Postgres"
+# setting environment to localhost
+environment = "localhost"
+
+# get username from credentials
+username = Cred.get_username(database, environment)
+# get password from credentials
+password = Cred.get_password(database, environment)
+# get host from credentials
+host = Cred.get_host(database, environment)
+# get database from credentials
+database = Cred.get_database(database, environment)
+# get port from credentials
+port = Cred.get_port(database, environment)
+
 
 # set up connection to postgres
 connection = psycopg2.connect(
-      host="localhost",
-      database="financial_db",
-      user="postgres",
-      password="postgres",
-      port=5432
+      host = host,
+      database = database,
+      user = username,
+      password = password,
+      port = port
   )
 
 # create cursor to execute queries with
