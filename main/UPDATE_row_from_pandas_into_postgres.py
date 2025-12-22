5194e9630c38364cc0031768a759f9cc6fab86b9
tim-kornish-cloud <kornish.timothy@gmail.com>
2025-12-21 23:33:12 -0700

    Update UPDATE_row_from_pandas_into_postgres.py
    updating update script to use function in custom db utils class

--- main/UPDATE_row_from_pandas_into_postgres.py
+++ main/UPDATE_row_from_pandas_into_postgres.py
@@ -47,15 +47,48 @@ cursor = connection.cursor()
 
 # set update syntax for specifically the amount on a users account
 # modify to be more dynamic later once update basic concept passing
-update_record_sql = """
-UPDATE transactions
-SET Amount = %s
-WHERE userId = %s
-"""
+# select accounts to match against the csv to not attempt to insert duplicates
+select_query = """SELECT TOP (1000) [AccountNumber]
+      ,[Name]
+      ,[NumberOfEmployees]
+      ,[NumberOfLocations__c]
+      ,[Phone]
+      ,[SLA__c]
+      ,[SLASerialNumber__c]
+      ,[Account_Number_External_ID__c]
+  FROM [Data_Engineering].[dbo].[Accounts_test_1]"""
 
-# query the records from the postgres table, load into a DataFrames
-# get userId, then modify Amount column. grab values from dataframe and put in variables amount and user_id
+# accounts in the postgres table shown in the query above
+account_df = Postgres_Utils.query_postgres_return_dataframe(select_query, cursor)
 
-# execute the update against the dataframe values
-cursor.execute(update_record_sql, amount, user_id)
-connection.commit()
+# format the merge column to remove any whitespace
+account_df.columns = account_df.columns.str.strip()
+mock_data_df.columns = mock_data_df.columns.str.strip()
+
+# format the merge column data types to string before joining
+account_df['Account_Number_External_ID__c'] = account_df['Account_Number_External_ID__c'].astype(str)
+mock_data_df['Account_Number_External_ID__c'] = mock_data_df['Account_Number_External_ID__c'].astype(str)
+
+# get dataframes for records that exist in both df, left and right only
+# perform outer join on 'Account_Number_External_ID__c'
+both_df, left_only_df, right_only_df = Utils.get_df_diffs(account_df, mock_data_df, left_on = 'Account_Number_External_ID__c', right_on = 'Account_Number_External_ID__c', how = 'outer', suffixes = ('_left', '_right'), indicator = True, validate = None)
+
+
+# modify account SLA value to gold
+# modify number of locations to 5
+account_columns_to_update = ['SLA__c', 'NumberOfLocations__c']
+account_values_to_update = ["Gold", "25"]
+
+#set table to update
+table_to_update = '[Data_Engineering].[dbo].[Accounts_test_1]'
+
+#table key field
+table_UID = 'Account_Number_External_ID__c'
+
+# below is a sql list as a single string
+accounts_to_update_list = Utils.generate_sql_list_from_df_column(both_df, 'Account_Number_External_ID__c', output = 'string')
+
+# upload the update sql call to Postgres database
+Postgres_Utils.update_rows_in_postgres_table(connection, cursor, table_to_update,
+                                      account_columns_to_update, account_values_to_update,
+                                      table_UID, accounts_to_update_list)
