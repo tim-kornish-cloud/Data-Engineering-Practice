a133a1e3fafc2346a8f7d0dc00b48d8f22f99fa1
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-01-04 23:25:28 -0700

    changing update format to update multiple columns with with unique values in different rows, before could only set constant values

--- main/UPDATE_row_from_pandas_into_postgres.py
+++ main/UPDATE_row_from_pandas_into_postgres.py
@@ -14,6 +14,8 @@ from custom_db_utilities import  Postgres_Utilities, Custom_Utilities
 # retreive stored credentials
 from credentials import Credentials
 
+from psycopg2.extras import execute_values
+
 # create and instance of the custom postgres utilities class used to interact with postgres DB/tables
 Postgres_Utils = Postgres_Utilities()
 # create and instance of the custom  utilities class
@@ -58,20 +60,53 @@ select_query = """SELECT accountnumber,
 account_df = Postgres_Utils.query_postgres_return_dataframe(select_query, connection)
 
 # modify account SLA value to gold
+account_df['sla__c'] = "Gold"
 # modify number of locations to 25
+account_df['numberoflocations__c'] = "25"
+
+#list all columns to be updated in this call
 account_columns_to_update = ['sla__c', 'numberoflocations__c']
-account_values_to_update = ["Gold", "25"]
 
 # set table to update
-table_to_update = 'accounts_test'
+table_name = 'accounts_test'
 
 # table key field
-table_UID = 'account_number_external_id__c'
+table_uid = 'account_number_external_id__c'
+# table key field in dataframe, in case there is a mismatch
+df_uid = 'account_number_external_id__c'
 
 # below is a sql list as a single string
 accounts_to_update_list = Utils.generate_sql_list_from_df_column(account_df, 'account_number_external_id__c', output = 'string')
 
-# upload the update sql call to Postgres database
-Postgres_Utils.update_rows_in_postgres_table(connection, cursor, table_to_update,
-                                      account_columns_to_update, account_values_to_update,
-                                      table_UID, accounts_to_update_list)
+# create beginning of update, add table name
+sql_update = f"UPDATE {table_name} SET "
+col_list = ""
+df_col_list = [df_uid]
+# loop through fields on df
+for col in account_columns_to_update:
+    # assume table and dataframe column names are identical, rename df column names if mismatch
+    sql_update = sql_update + f"{col} = data.{col}, "
+    col_list = col_list + col + ", "
+    df_col_list.extend([col])
+sql_update = sql_update[:-2]
+col_list = col_list[:-2]
+sql_update = sql_update + f" FROM (VALUES %s) AS data ({df_uid}, {col_list}) WHERE {table_name}.{table_uid} = data.{df_uid}"
+
+print(sql_update)
+print(df_col_list)
+df_to_update = account_df[df_col_list]
+print(df_to_update.head())
+
+# convert the rows in the dataframe into tuples
+data = [tuple(x) for x in df_to_update.values]
+
+# execute insert of records
+execute_values(cursor, sql_update, data)
+
+# commit the sql statement
+connection.commit()
+
+# # upload the update sql call to Postgres database
+# Postgres_Utils.update_rows_in_postgres_table(connection, cursor, table_to_update,
+#                                       account_columns_to_update, account_values_to_update,
+#                                       table_UID, accounts_to_update_list)
