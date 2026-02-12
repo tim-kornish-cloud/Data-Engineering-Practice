84930521e9f805b0d58de5b97bad43796a0d7c02
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-02-11 21:12:56 -0700

    set up initial insert to test select query on connection in python script

---
+++ main/snowflake_sql_scripts/insert_row_account_test.sql
@@ -0,0 +1,4 @@
+INSERT INTO ACCOUNT_DB.PUBLIC.ACCOUNTS_TEST
+(AccountNumber,Name,NumberOfEmployees,NumberOfLocations__c,Phone,SLA__c,SLASerialNumber__c,Account_Number_External_ID__c,IsActive,CreatedDate,AmountPaid)
+VALUES
+('1','Berni','19','50','801-507-7120','gold','242272447','7d578aec-9b1d-4cdc-9dc3-f87fd1ced883','false','4/9/2025','30.65');
